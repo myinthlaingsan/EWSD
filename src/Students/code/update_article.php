dda2cfd7eb1998e0cbea58c17558752658053790
DESKTOP-NGG5VBK\RH_Computer <myinthlaingsan747@gmail.com>
2025-04-21 18:57:03 +0630

    debug update article

--- src/Students/code/update_article.php
+++ src/Students/code/update_article.php
@@ -19,6 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         "article_id" => $article_id,
         "title" => $title,
         "status" => $status,
+        "academicyear" => $academicyear,
     ]);
 
     if (!$updated) {
